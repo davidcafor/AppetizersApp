ae9e99446699167888acd4b2c7b5d7914d3e5020
davidcafor <davidcafor@gmail.com>
2024-04-18 17:42:34 +0200

    Initial commit

---
+++ AppetizersApp/AppetizersAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersAppApp.swift
+//  AppetizersApp
+//
+//  Created by David Cardeña on 18/4/24.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
